b90566548f99d5488e15a1cda007ea10d92b5dc0
lafond <macklafonddeboumas@gmail.com>
2025-12-17 13:53:47 +0100

    end of feature product-context

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,10 @@
-import { useEffect, useState } from "react";
 import ProductList from "./components/ProductLists";
 
 const App = () => {
-  const [products, setProducts] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    const fetchProducts = async () => {
-      try {
-        const res = await fetch("api/products");
-        if (!res.ok) throw new Error("Failed to Fetch Products");
-        const data = await res.json();
-        console.log(data);
-        setProducts(data);
-      } catch (err) {
-        setError(err.message);
-      } finally {
-        setLoading(false);
-      }
-    };
-    fetchProducts();
-  }, []);
-
   return (
     <div className="min-h-screen bg-gray-100 p-6">
       <h1 className="text-3xl font-bold mb-6"> 🛒 Shopping Cart</h1>
-      {loading && <p>Loading....</p>}
-      {error && <div className="error">{error}</div>}
-      <ProductList products={products} />
+      <ProductList />
     </div>
   );
 };
---
+++ src/context/ProductContext.jsx
@@ -0,0 +1,36 @@
+import { createContext, useContext, useEffect, useState } from "react";
+
+const ProductContext = createContext();
+
+export function ProductProvider({ children }) {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const res = await fetch("api/products");
+        if (!res.ok) throw new Error("Failed to Fetch Products");
+        const data = await res.json();
+        console.log(data);
+        setProducts(data);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchProducts();
+  }, []);
+
+  return (
+    <ProductContext.Provider value={{ products, loading, error }}>
+      {children}
+    </ProductContext.Provider>
+  );
+}
+
+export function useProducts() {
+  return useContext(ProductContext);
+}
