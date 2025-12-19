4dc6289e01e864c003ae729291db3fc7c362498f
lafond <macklafonddeboumas@gmail.com>
2025-12-19 17:43:35 +0100

    End of feature header Cart Count

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
+import Header from "./components/Header";
 import ProductList from "./components/ProductLists";
 
 const App = () => {
   return (
-    <div className="min-h-screen bg-gray-100 p-6">
-      <h1 className="text-3xl font-bold mb-6"> 🛒 Shopping Cart</h1>
-      <ProductList />
-    </div>
+    <>
+      <Header />
+      <div className="min-h-screen bg-gray-100 p-6">
+        <h1 className="text-3xl font-bold mb-6"> 🛒 Shopping Cart</h1>
+        <ProductList />
+      </div>
+    </>
   );
 };
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,23 @@
+import { FaShoppingCart } from "react-icons/fa";
+import { useCart } from "../context/CartContext";
+
+const Header = () => {
+  const { cart } = useCart();
+  const itemCount = cart.reduce((acc, item) => acc + item.qty, 0);
+
+  return (
+    <header className="bg-white shadow-md p-4 flex justify-between items-center">
+      <h1 className="text-2xl font-bold text-blue-600">Shop Mate</h1>
+      <div className="relative">
+        <FaShoppingCart className="text-2xl text-gray-700" />
+        {itemCount > 0 && (
+          <span className="absolute bg-red-500 -top-2 -right-2 text-white text-xs w-5 h-5 flex item-center justify-center rounded-full ">
+            {itemCount}
+          </span>
+        )}
+      </div>
+    </header>
+  );
+};
+
+export default Header;
