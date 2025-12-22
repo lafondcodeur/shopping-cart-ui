7face886cd7218be111247bda9b70824f58eb9fb
lafond <macklafonddeboumas@gmail.com>
2025-12-22 17:23:17 +0100

    Last commit for this project

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,11 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    outDir: "build",
+    assetsDir: "assets2",
+    minify: false,
+  },
   server: {
     proxy: {
       "/api": {
